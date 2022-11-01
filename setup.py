278454b41b0259b5413c9c58d0395e5f72ec2bff
yaswanthnilla <20131a4441@gvpce.ac.in>
2022-11-01 21:50:03 +0530

    Added files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="sensor",
+    version="0.0.1",
+    author="yaswanth",
+    author_email="yaswanth9220@gmail.com",
+    packages=find_packages(),
+
+)
